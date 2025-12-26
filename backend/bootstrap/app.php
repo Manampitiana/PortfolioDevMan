ad0eb77561a20cbc780d254f45120286653039fc
Tsiry <manampitianatsiriniaina@gmail.com>
2025-12-26 11:54:11 +0300

    Fanazavana fohy momba ny modification nataoko

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
+
+       // Register global middleware
+       $middleware->prepend(CorsMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
