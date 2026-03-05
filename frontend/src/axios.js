e15d808658f1c3c27871aa56dbb81c67f4bd023a
Tsiry <manampitianatsiriniaina@gmail.com>
2026-03-05 08:46:51 +0300

    modifications cors and .env

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,21 +2,24 @@ import axios from "axios";
 
 const axiosClient = axios.create({
     baseURL: "https://portfolio-backend-58gy.onrender.com/api",
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
 });
 
 axiosClient.interceptors.request.use((config) => {
     const token = localStorage.getItem("ACCESS_TOKEN");
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
 });
 
 axiosClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
+    (response) => response,
     (error) => {
         if (error.response && error.response.status === 401) {
-            // Clear token and redirect to login
             localStorage.removeItem("ACCESS_TOKEN");
             window.location.href = "/admin/login";
         }
