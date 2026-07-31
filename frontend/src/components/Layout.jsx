30ed8c3dc55b851204a1cb884b3be4336ed66a30
Tsiry <manampitianatsiriniaina@gmail.com>
2026-07-31 08:10:03 +0300

    modif page

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Footer from './Footer'
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col overflow-x-hidden">
       <Navigation />
       <main className="flex-grow">
         {children}
