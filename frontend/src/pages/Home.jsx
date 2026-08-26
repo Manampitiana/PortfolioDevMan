470d8f82c8f1ec3cd00b37681e8344c3415c6cf1
Tsiry <manampitianatsiriniaina@gmail.com>
2026-08-26 15:36:25 +0300

    modif mobilnav

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -220,6 +220,7 @@ export default function Home() {
               </div>
 
               <OsTaskbar />
+               <OsMobileNav />
             </section>
 
             {/* Section Compétences */}
