cbadd2a31def21b615ec7866ae4328ac31c77be4
Tsiry <manampitianatsiriniaina@gmail.com>
2026-08-27 09:20:41 +0300

    add src

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -235,7 +235,7 @@ export default function Home() {
                 </div>
               </div>
 
-              <OsTaskbar />
+              {/* <OsTaskbar /> */}
               <OsMobileNav />
             </section>
 
